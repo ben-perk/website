1bb01910a5521eaaaafda6989680eccb3223ed52
ben.perk <ben.perkton@gmail.com>
2025-01-30 11:44:16 -0500

    playing with hero

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#contact-form');
-  if (!form) {
-    console.error('Form element not found');
-    return;
-  }
+//   if (!form) {
+//     console.error('Form element not found');
+//     return;
+//   }
 
   form.addEventListener('submit', (event) => {
     event.preventDefault(); // Prevent default form submission
@@ -28,4 +28,3 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('LinkedIn:', linkedinInput);
     console.log('Job:', jobInput);
   });
-});
